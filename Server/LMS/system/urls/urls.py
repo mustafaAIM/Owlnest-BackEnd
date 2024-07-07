73b74db2db901b6475654f46214a4c9c210f5160
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-07-07 18:11:47 +0300

    urls update

--- Server/LMS/system/urls/urls.py
+++ Server/LMS/system/urls/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 #views 
 from system.views.CourseReportView import CourseReportView
 from system.views.Comment import ListCreateCommentView
-
+from system.views.MarkContentAsCompleted import MarkContentView
 #DRF
  
 urlpatterns = [
   path('course/<id>/report',CourseReportView.as_view()),
-  path('course/<int:id>/comments',ListCreateCommentView.as_view())
+  path('course/<int:id>/comments',ListCreateCommentView.as_view()),
+  path('mark-content-completed' , MarkContentView.as_view()),
+
+
 ]
+
  
